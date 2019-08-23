047cb891c2674a420a241106c106e555d6415b8e
kim <36021068+kangjinshun@users.noreply.github.com>
2019-08-23 13:04:53 +0800

    test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * 〈一句话功能简述〉<br>
+ *
+ * @author kangjinshun
+ * @create 2019/8/23
+ * @since 1.0.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" hello !");
+    }
+}
